3330e6d3e2585b7a1a1ca5e7307b6d36ead42c67
rominalodolo <lodoloromina@gmail.com>
2022-04-06 09:05:25 +0200

    update

--- Classwork/src/main/java/Lesson5/Television.java
+++ Classwork/src/main/java/Lesson5/Television.java
@@ -1,4 +1,13 @@
 package Lesson5;
 
-public class Television {
+public class Television extends ElectronicDevice{
+    @Override
+    public void turnOn(){
+        initialize();
+        System.out.println("Turned On!");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("Turned Off!");
+    }
 }
