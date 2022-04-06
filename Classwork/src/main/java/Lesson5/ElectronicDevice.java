335340d08d4922f4a1b4ab2b43e95c5750bfe7be
rominalodolo <lodoloromina@gmail.com>
2022-04-06 09:03:16 +0200

    new

--- Classwork/src/main/java/Lesson5/ElectronicDevice.java
+++ Classwork/src/main/java/Lesson5/ElectronicDevice.java
@@ -1,4 +1,28 @@
 package Lesson5;
 
-public class ElectronicDevice {
+public abstract class ElectronicDevice {
+    public void initialize() {
+        System.out.println("Initializing...");
+    }
+
+    public void setState(PowerState state){
+        switch(state){
+            case ON:
+                System.out.println(PowerState.ON);;
+                break;
+            case OFF:
+                System.out.println(PowerState.OFF);;
+                break;
+            case SUSPENDED:
+                System.out.println(PowerState.SUSPENDED);;
+                break;
+
+        }
+    }
+    public void getState(){
+
+    }
+    public abstract void turnOn();
+
+    public abstract void turnOff();
 }
