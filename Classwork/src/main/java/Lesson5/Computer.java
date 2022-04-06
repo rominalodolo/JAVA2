eef2b860070b53cf195593d69a6a83a1420c545b
rominalodolo <lodoloromina@gmail.com>
2022-04-06 09:04:24 +0200

    update

--- Classwork/src/main/java/Lesson5/Computer.java
+++ Classwork/src/main/java/Lesson5/Computer.java
@@ -1,4 +1,13 @@
 package Lesson5;
 
-public class Computer {
+public class Computer extends ElectronicDevice {
+    @Override
+    public void turnOn(){
+        initialize();
+        System.out.println("Turned On!");
+    }
+    @Override
+    public void turnOff(){
+        System.out.println("Turned Off!");
+    }
 }
